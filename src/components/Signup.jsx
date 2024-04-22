8f0a49b380960ebcbe9e0dcf6501d4b7c58ee603
mshah03 <manthanshah2003@gmail.com>
2024-04-22 20:00:56 +0530

    Validations and Logout

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -10,6 +10,7 @@ function Signup(props) {
   });
 
   const [cPassword, setCPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   function handleChange(e) {
     let name = e.target.name;
@@ -23,31 +24,40 @@ function Signup(props) {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (user.email && user.password) {
-      if (user.password === cPassword) {
-        try {
-          const response = await fetch(`http://localhost:3001/api/auth/register`, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(user),
-          });
-          if (!response.ok) {
-            alert("User already registered! Please Login.");
-          }
-          else {
-            alert("Registration Successful. Please Login.")
-          }
-          window.location.reload();
-        } catch (error) {
-          console.error("Registration error:", error);
-        }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(user.email)) {
+      setErrorMessage("Invalid email format.");
+      return;
+    }
+
+    if (user.password.length < 8) {
+      setErrorMessage("Password must be at least 8 characters long.");
+      return;
+    }
+
+    if (user.password !== cPassword) {
+      setErrorMessage("Password and confirm password don't match.");
+      return;
+    }
+
+    // Submit the form
+    try {
+      const response = await fetch(`http://localhost:3001/api/auth/register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+      if (!response.ok) {
+        alert("User already registered! Please Login.");
       } else {
-        alert("Password and confirm password don't match.");
+        alert("Registration Successful. Please Login.")
       }
-    } else {
-      alert("Please enter the email, password, and confirm password.");
+      window.location.reload();
+    } catch (error) {
+      console.error("Registration error:", error);
     }
   };
 
@@ -55,6 +65,7 @@ function Signup(props) {
   return (
     <div className="form-inner">
       <form className="login">
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
         <div className="field">
           <input
             type="email"
